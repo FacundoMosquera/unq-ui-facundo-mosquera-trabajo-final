85fea0a12fb9ad48daf17a2aa1f87a982946614e
Facundo Mosquera <facundomosquerabus@gmail.com>
2025-11-29 03:56:23 -0300

    preguntados jugable

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,8 +8,9 @@ import { usePreguntas } from "../../contexts/Preguntas";
 export default function Menu() {
     const [dificultades, setDificultades] = useState([]);
     const [loading, setLoading]           = useState(true);
-    const [preguntas, setPreguntas]       = useState([]);
-    const { setDificultad } = usePreguntas();
+    const [preguntas, setPreguntas]       = useState(null);
+    const [respondio, setRespondio]       = useState(false);
+    const { setDificultad, preguntasTotales, setPreguntasTotales, setPreguntaActual, preguntaActual, preguntasAcertadas, setPreguntasAcertadas } = usePreguntas();
     
     useEffect(() => {
         fetch("https://preguntados-api.vercel.app/api/difficulty")
@@ -27,27 +28,86 @@ export default function Menu() {
         .then(res => res.json())
         .then(data => {
             setPreguntas(data);
-            console.log(data)
+            setPreguntasTotales(data.length);
+            setPreguntaActual(1);
         })
         .catch(err => console.log("Ocurrió un error al recuperar las preguntas", err))
         .finally(() => setLoading(false) );
     }
+
+    const opciones = (pregunta) => Object.entries(pregunta)
+    .filter(([key]) => key.startsWith("option"));
+
+    const handleAnswer = (id, option) => {
+        fetch(`https://preguntados-api.vercel.app/api/answer`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                questionId: id,
+                option: option
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            data.answer ? setPreguntasAcertadas(prev => prev + 1) : null;
+            setRespondio(true);
+            })
+        .catch(err => console.log("Ocurrió un error al enviar la respuesta", err))
+    }
+
+    const handleNextQuestion = () => {
+        if(preguntaActual === preguntasTotales) {
+
+        } else {
+            setPreguntaActual(prev => prev + 1);
+            setRespondio(false);
+        }
+    }
     
-    return(
-        
-        <div className="menu">
-            <div className="bienvenida">
-                <h1>Preguntados</h1>
+    if(!preguntas) {
+        return(
+            <div className="menu">
+                <div className="bienvenida">
+                    <h1>Preguntados</h1>
+                </div>
+                <div className="dificultades">
+                    {loading ? (<p>CARGANDO...</p>) : (
+                        dificultades.map(dif => (
+                            <div key={dif} className="opcion" onClick={() => handleDifficulty(dif)}>
+                                <OpcionDificultad dificultad={dif}/>
+                            </div>
+                        ))
+                    )}
+                </div>
             </div>
-            <div className="dificultades">
-                {loading ? (<p>CARGANDO...</p>) : (
-                    dificultades.map(dif => (
-                        <div key={dif} className="opcion" onClick={() => handleDifficulty(dif)}>
-                            <OpcionDificultad dificultad={dif}/>
-                        </div>
-                    ))
-                )}
+        );
+    }
+
+
+    if(preguntas.length > 0 ) {
+        console.log(preguntas);
+        console.log(preguntasAcertadas)
+        return(
+            <div className="menu">
+                <div className="indicadores">
+                    <span>{preguntaActual}/{preguntasTotales}</span>
+                </div>
+                <div className="pregunta">
+                    <span>{preguntas[preguntaActual-1].question}</span>
+                    <div className="options">
+                        {opciones(preguntas[preguntaActual-1]).map(([key, value]) =>(
+                            <div id={key} key={key} className="option" onClick={() => respondio ? null : handleAnswer(preguntas[preguntaActual-1].id, key)}>
+                                <span>{value}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="footer">
+                    <button onClick={handleNextQuestion} disabled={!respondio}>Siguiente pregunta</button>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 }
--- src/contexts/Preguntas.jsx
+++ src/contexts/Preguntas.jsx
@@ -5,26 +5,31 @@ import { createContext, useContext, useState } from "react";
 const PreguntasContext = createContext({
     preguntasTotales: 0,
     preguntasAcertadas: 0,
+    preguntaActual: 0,
     dificultad: "easy",
     setPreguntasAcertadas: () => {},
     setPreguntasTotales: () => {},
-    setDificultad: () => {}
-    
+    setPreguntaActual: () => {},
+    setDificultad: () => {},
+
 });
 
 
 export const PreguntasProvider = ({children}) => {
     const [preguntasTotales, setPreguntasTotales]     = useState(0);
     const [preguntasAcertadas, setPreguntasAcertadas] = useState(0);
+    const [preguntaActual, setPreguntaActual]         = useState(0);
     const [dificultad, setDificultad] = useState("easy");
 
     const values = {
         preguntasTotales: preguntasTotales,
         preguntasAcertadas: preguntasAcertadas,
+        preguntaActual: preguntaActual,
         dificultad: dificultad,
         setPreguntasAcertadas: setPreguntasAcertadas,
         setDificultad: setDificultad,
-        setPreguntasTotales: setPreguntasTotales
+        setPreguntasTotales: setPreguntasTotales,
+        setPreguntaActual: setPreguntaActual
     }
 
     return (
